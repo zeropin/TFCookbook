959741fa378e7e817fc3359643e13625a0a82fd0
zeropin <zzuo@stanford.edu>
2020-02-23 20:22:42 -0800

    Change the color scheme.

--- R/plot_functions.R
+++ R/plot_functions.R
@@ -1,6 +1,9 @@
 plotEnergyLogo <- function(energyMatrix) {
+  default_colors = ggseqlogo::make_col_scheme(chars=c('A', 'C', 'G', 'T', 'M', 'W', 'N'),
+                             cols=c("#0E927B", "#59A9D8", "#DC9514", "#1A1A1A", "#FF0000", "#FF0000", "#5C5C5D"))
+  
   ggplot2::ggplot() +
-    ggseqlogo::geom_logo(-energyMatrix, method = "custom", seq_type = "dna") +
+    ggseqlogo::geom_logo(-energyMatrix, method = "custom", seq_type = "dna", col_scheme = default_colors) +
     ggseqlogo::theme_logo() +
     ggplot2::xlab("Position") +
     ggplot2::ylab("-Energy (kT)") %>%
