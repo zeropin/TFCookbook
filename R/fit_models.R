fc1f03bcb62e1e28d9aba2224bf87cd560cd1eb1
zeropin <zzuo@stanford.edu>
2020-02-23 20:23:53 -0800

    Fix predictEnergy function.

--- R/fit_models.R
+++ R/fit_models.R
@@ -27,8 +27,12 @@ buildEnergyModel_MW <- function(data) {
 }
 
 predictEnergy <- function(sequences, model) {
-  num <- (model$rank - 1) / 3
-
+  #num <- (model$rank - 1) / 3  
+  num <- nchar(sequences[[1]]) 
+  
+  #num shoulbe be the number of nucleotide positions used for prediction，model$rank is the number of coefficients in the models.
+  # In some instances, there are NA values for some parameters, which are not included in total rank, so nchar(sequence) is used instead.
+  
   tibble(ByteCode = SeqToByte(sequences)) %>%
     tidyr::separate(ByteCode,
       into = paste0(rep(1:num, each = 3), c("CG", "CA", "CT")),
