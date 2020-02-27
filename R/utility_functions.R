e3d682cf7eb52a45b2ccc4d46e78ede18baa20dd
Zheng Zuo <zeropin@Zhengs-Mac-Mini.local>
2020-02-27 15:54:32 -0800

    Add roxygen documentation to utility_functions.R

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -1,3 +1,11 @@
+#' Count the number of mismatches of sequence(s) to some reference site
+#' 
+#' @param sequences List of sequences.
+#' @param reference Reference sequence.
+#' @return The number of mismatches between \code{sequences} and \code{reference}.
+#' @examples
+#' countMismatch("GTAC", "GAAC")
+#' countMismatch(c("GTAC", "GAAC"), "GAAC")
 countMismatch <- function(sequences, reference){
   seqs = strsplit(sequences, "")
   ref  = strsplit(reference, "")[[1]]
@@ -6,6 +14,15 @@ countMismatch <- function(sequences, reference){
                  function(seq) {sum(seq!=ref, na.rm=TRUE)})
 }
 
+
+#' Highlight the mismatches of sequence(s) to some reference site with '-'
+#' 
+#' @param sequences List of sequences.
+#' @param reference Reference sequence.
+#' @return Sequences with those mismatched portions to reference changed to '-'
+#' @examples
+#' highlightMismatch("GTAC", "GAAC")
+#' highlightMismatch(c("GTAC", "GAAC"), "GAAC")
 highlightMismatch<- function(sequences, reference){
   seqs = strsplit(sequences, "")
   ref  = strsplit(reference, "")[[1]]
@@ -17,6 +34,17 @@ highlightMismatch<- function(sequences, reference){
                  })
 }
 
+#' Convert nucleotide sequence to binary code based on Bases->Codes correspondence
+#' 
+#' @param sequences List of sequences.
+#' @param Bases List of Bases to be mapped
+#' @param Codes List of Binary codes corresponding to Bases
+#' @return Binary codes converted from sequences
+#' @examples
+#' SeqToByte("GTAC")
+#' SeqToByte("GTAAN",
+#' Bases=cbind('A', 'C', "G", "T", "N"),
+#' Codes=cbind("0 1 0 ", "0 0 0 ", "1 0 0 ", "0 0 1 ", "0.25 0.25 0.25 "))
 SeqToByte <- function(sequences,
                       Bases=cbind('A', 'C', "G", "T", "N"),
                       Codes=cbind("0 1 0 ", "0 0 0 ", "1 0 0 ", "0 0 1 ", "0.25 0.25 0.25 ") ){
@@ -30,7 +58,12 @@ SeqToByte <- function(sequences,
   )
 }
 
-
+#' Convert energy matrix to energy model coefficients
+#' 
+#' @param energyMatrix
+#' @return energy model coefficients
+#' @examples
+#' toCoeffs("GTAC")
 toCoeffs <- function(energyMatrix) {
   coeff <- array(0L, dim = 3 * ncol(energyMatrix))
   names(coeff) <- paste0(rep(1:ncol(energyMatrix), each = 3), c("CG", "CA", "CT"))
@@ -44,7 +77,12 @@ toCoeffs <- function(energyMatrix) {
   return(coeff)
 }
 
-
+#' Enumerate all k-mer sequences
+#' 
+#' @param k
+#' @return all \code{k}-mer sequences
+#' @examples
+#' kmer(6)
 kmer <- function(k){
   if(k==1)
     return(c("A", "C", "G", "T"))
@@ -55,6 +93,14 @@ kmer <- function(k){
   )
 }
 
+#' Select sequence variants based on the max number of mismatches to some reference site
+#' 
+#' @param data The data frame includes Sequence variable
+#' @param reference Reference site
+#' @param maxMismatches The max number of mismatches of each sequence to the reference site
+#' @return The data frame containing all sequences with no more than \code{maxMismatches} to the reference
+#' @examples
+#' selectVariants(data, "GTACGAC", maxMismatches=1)
 selectVariants <- function(data, reference, maxMismatches = 1) {
   return(dplyr::filter(data, countMismatch(Sequence, reference) <= maxMismatches))
 }
